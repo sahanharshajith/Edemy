380586039b5d385a3e922159cb0ba5442cd40612
sahanharshajith <sahan1234harshajith@gmail.com>
2025-04-09 15:21:11 +0530

    Add Clerk authentication and update Navbar component
    - Integrated ClerkProvider in main.jsx for authentication management.
    - Added Navbar component with user authentication features.
    - Updated package.json and package-lock.json to include @clerk/clerk-react dependency.
    - Created .env file for storing the Clerk publishable key.

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import logo from '../../assets/logo.svg';
+import usericon from '../../assets/user_icon.svg';
+import { Link } from 'react-router-dom'
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
 
 const Navbar = () => {
+
+  const isCourseListPage = location.pathname.includes('/course-list');
+
+  const {openSignIn} = useClerk();
+  const {user} = useUser();
+
   return (
-    <div>
-        <h1>Navbar</h1>
+    <div className={`flex justify-between items-center px-4 sm:px-10 md:px-14 lg:px-36 border-b
+    border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
+        <img src={logo} alt="Logo" className='w-28 lg:w-32 cursor-pointer'/>
+        <div className='hidden md:flex items-center gap-5 text-gray-500'>
+          <div className='flex items-center gap-5'>
+            { user && <>
+              <button>Become Educator</button> | 
+            <Link to="/my-enrollment"> My Enrollments</Link>
+            </>
+            }
+          </div>
+          { user ? <UserButton /> :
+          <button onClick={() => openSignIn()} className='bg-blue-600 text-white px-5 py-2 
+          rounded-full cursor-pointer'>Create Account</button>}
+        </div>
+
+        <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'> 
+          <div className='flex items-center gap-1 sm:gap-2 max-sm:text:xs'>
+            { user && <>
+              <button>Become Educator</button> | 
+            <Link to="/my-enrollment"> My Enrollments</Link>
+            </> 
+            }
+          </div>
+          {
+            user? <UserButton /> : <button onClick={() => openSignIn()}><img src={usericon} alt="User Icon" className='w-8 h-8 cursor-pointer'/></button>
+          }
+        </div>
     </div>
   )
 }
