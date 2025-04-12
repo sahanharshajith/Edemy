07c949fef9271414fd4c53055c5c3aa5a409a14e
sahanharshajith <sahan1234harshajith@gmail.com>
2025-04-12 20:50:50 +0530

    Add TestimonialsSection component to Home page and enhance layout with testimonials display

--- client/src/components/student/TestimonialsSection.jsx
+++ client/src/components/student/TestimonialsSection.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React from 'react';
+import { dummyTestimonial } from '../../assets/assets';
+import star from '../../assets/star.svg';
+import star_dull from '../../assets/star_dull_icon.svg';
 
 const TestimonialsSection = () => {
   return (
-    <div>
-        <h1>Testimonials Section</h1>
-    </div>
-  )
-}
+    <div className='pb-14 px-8 md:px-0 max-w-6xl mx-auto'>
+      <h2 className='text-3xl font-medium text-gray-800'>Testimonials</h2>
+      <p className='md:text-base text-gray-500 mt-3'>
+        Hear from our learners as they share their journeys of transformation, success, 
+        and how our platform has made a difference in their lives.
+      </p>
 
-export default TestimonialsSection
+      <div className='flex gap-8 mt-14 overflow-x-auto pb-4'>
+        {dummyTestimonial.map((testimonial, index) => (
+          <div 
+            key={index} 
+            className='text-sm text-left border border-gray-500/30 pb-6 min-w-[300px] md:min-w-0
+            rounded-lg bg-white shadow-[0px_4px_15px_0px] shadow-blue-500/10 overflow-hidden'
+          >
+            <div className='flex items-center gap-4 px-5 py-4 bg-gray-500/10'>
+              <img 
+                className='h-12 w-12 rounded-full object-cover' 
+                src={testimonial.image} 
+                alt={testimonial.name} 
+              />
+              <div>
+                <h1 className='text-lg font-medium text-gray-800'>{testimonial.name}</h1>
+                <p className='text-gray-800/80'>{testimonial.role}</p>
+              </div> 
+            </div>
+            <div className='p-5 pb-7'>
+              <div className='flex gap-0.5'>
+                {[...Array(5)].map((_, i) => (
+                  <img 
+                    className='h-5' 
+                    key={i} 
+                    src={i < testimonial.rating ? star : star_dull} 
+                    alt="star" 
+                  />
+                ))}
+              </div>
+              <p className='text-gray-500 mt-5'>{testimonial.feedback}</p>
+            </div> 
+            <a href="#" className='text-blue-500 underline px-5'>Read More</a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialsSection;
